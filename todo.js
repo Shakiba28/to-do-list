013de78f566ae8873afcfe6b2a8cc9fe64d91676
Shakiba <shakibabaghishani@gmail.com>
2022-07-18 18:12:15 +0430

    add todo list to ripo

---
+++ todo.js
@@ -0,0 +1,58 @@
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+//add task 
+function add() {
+    var task = document.getElementById('task').value;
+    if (task === "") {
+        alert("enter your task !")
+    } else {
+        var todos = get_todos();
+        todos.push(task);
+        alert("Added task ✔");
+        localStorage.setItem('todo', JSON.stringify(todos));
+
+        show();
+
+        return false;
+    }
+
+}
+//remove task
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    alert("task remove!");
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var todos = get_todos();
+
+    var html = '<ul class="bg-warning list-unstyled ">';
+    for (var i = 0; i < todos.length; i++) {
+        html += '<li class="list">' + todos[i] + '<i class="bi bi-trash-fill remove" id="' + i + '"></i></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+        
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
